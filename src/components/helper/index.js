eee0bb61ed334c790a8be4d6b07db236e7b3fc7f
Alex Merrifield <alex.merrifield@levelupdevelopment.com>
2022-09-09 11:58:50 -0400

    init

---
+++ src/components/helper/index.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { View, Animated } from 'react-native';
+
+import styles from './styles';
+
+export default class Helper extends PureComponent {
+  static defaultProps = {
+    numberOfLines: 1,
+  };
+
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node,
+    ]),
+  };
+
+  render() {
+    let { children, style, ...props } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Animated.Text style={[styles.text, style]} {...props}>
+          {children}
+        </Animated.Text>
+      </View>
+    );
+  }
+}
